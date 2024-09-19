4ae3aa4452a0235cbee0f92b0131a7a683c8b6e8
Ivan Yao <yaoivan2002@gmail.com>
2024-09-19 09:43:51 +0000

    Ajout des viewsets

--- src/api/viewsets/exercise_viewset.py
+++ src/api/viewsets/exercise_viewset.py
@@ -1,7 +1,15 @@
 from rest_framework import viewsets
 from ..serializers.exercise_serializer import ExerciseSerializer
 from exercise.models.exrcise_model import ExerciseModel
+from rest_framework import status
+from rest_framework.response import Response
 
 class ExerciseViewSet(viewsets.ModelViewSet):
     queryset = ExerciseModel.objects.all()
     serializer_class = ExerciseSerializer
+
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+        instance.status = False
+        instance.save()
+        return Response(status=status.HTTP_204_NO_CONTENT)
